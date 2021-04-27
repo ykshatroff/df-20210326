26a2a55d90502e3a9e5a32ac67fe7ff0e93d6969
Yuriy Shatrov <yks-uno@ya.ru>
2021-04-27 10:04:15 +0300

    Test database

--- df_20210326/accounts/tests.py
+++ df_20210326/accounts/tests.py
@@ -1,5 +1,19 @@
 from django.conf import settings
 
+import pytest
+
+from accounts.models import create_database, list_databases
+
+pytestmark = pytest.mark.django_db
+
 
 def test_custom_user_model():
     assert settings.AUTH_USER_MODEL == "accounts.User"
+
+
+def test_database_created():
+    db_list = list_databases()
+    assert not db_list
+    create_database('user_1', 'password1')
+    db_list = list_databases()
+    assert db_list == ['user_1']
